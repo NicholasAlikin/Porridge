0d4cf3d1f2ef3a5a88cc54de3881e09c85b40e21
Alikin Nikolay <nickalikin@gmail.com>
2025-01-27 17:40:48 +0300

    Nonlinear static analysis with path continuation procedure

--- cpp/fem/include/staticNL.hpp
+++ cpp/fem/include/staticNL.hpp
@@ -1,34 +1,104 @@
-// #pragma once
+#pragma once
 
-// #include "corrector.hpp"
-// #include <functional>
-// #include "fem.hpp"
+// std headers
 
-// namespace fem {
+// local headers
+#include "linalg.hpp"
+
+#include "basic_system.hpp"
+
+#include "fem.hpp"
+#include "analyzes.hpp"
+
+// other headers
 
 
-// class StaticNL: public math::Corrector {
-// private:
-// public:
+namespace fem::npath {
+// namespace npath {
+/*Main euqation:
+    h(q,l) = l*g - f(q) = 0
+where
+    q - displacement vector
+    l - load factor ()
+    g - unitial external load vector
+*/
+class StaticNL: public ::npath::BasicSystem {
+private:
+public:
     
+    Model model;
+    Assemble assemble;
     
-//     StaticNL() = default;
+    math::vector<double> loadExt;        // external load, |loadExt| == 1
+    math::vector<double> loadInt;
+    math::vector_t<double,3> Rsum;
+    math::vector<size_t> ynorm_pos; // displacement vector components, which need to print
 
-//     math::Vector_t analysis();
-
-//     void system_response(math::Vector_t& fun, math::Matrix_t& jac, const math::Vector_t& y) const override;
-//     void system_response(math::Vector_t& fun, math::Matrix_t& jac, const math::Vector_t& y, const math::Vector_t& Dy) const override;
-//     void system_response_ext(math::Vector_t& fun, math::Matrix_t& jac, const math::Vector_t& y) const override;
-//     void system_response_ext(math::Vector_t& fun, math::Matrix_t& jac, const math::Vector_t& y, const math::Vector_t& Dy) const override;
-
-//     // void correction_system_response(Vector_t& fun, Matrix_t& jac, const Vector_t& y) override;
-//     // void continuation_system_response(Vector_t& fun, Matrix_t& jac, const Vector_t& y, const Vector_t& Dy) override;
-
+    double loadExt_norm;
     
-    
-//     math::Vector_t system(const math::Vector_t& x, const math::Matrix_t& L, const math::Vector_t& fnl, const math::Vector_t& fex) const;
-//     void system_jac(math::Matrix_t& jac, math::Vector_t& x, double freq, const math::Matrix_t& L, const math::Vector_t& fnl) const;
-//     void system_jac_load(math::Vector_t& jac, const math::Vector_t& x, double freq, const math::Vector_t& fnl, const math::Vector_t& fex) const;
-// };
+    math::vector_t<double,4> Rsum_continuation; // Rsum values for Continuation::continuation.process() for each curve point
 
-// } // namespace math
+    math::vector<double> loadExt_unit;
+private:
+    math::vector<double> temp_Dy;
+    math::vector<double> temp_theta;
+	math::vector_t<double,2> temp_rotTensor;
+	math::vector_t<double,2> temp_Rsumi;
+public:
+
+    StaticNL() = default;
+    
+    StaticNL(const StaticNL& other);
+    
+    StaticNL(StaticNL&& other);
+    
+    StaticNL(const Model& model
+            ,const Assemble& assemble
+            ,const math::vector<size_t>& ynorm_pos);
+    
+    void initialization();
+    
+    // void process_initialization();
+    void continuation_initialization(math::vector_t<double,2>& jac
+                                    ,math::vector<double>& ynorm);
+
+
+    void process_total_increment(math::vector<double>& y
+                               , math::vector<double>& dy);
+
+    void process_total_increment(math::vector<double>& Dy
+                               , math::vector<double>& dy
+                         , const math::vector<double>& predictor
+                        ,                     double   ds);
+
+
+    void system_response(math::vector<double>&     fun
+                       , math::vector_t<double,2>& jac
+                       , math::vector<double>&     y);
+    void system_response_extended(math::vector<double>& fun
+                                , math::vector_t<double,2>& jac
+                                , math::vector<double>& y);
+    
+
+    void calculate_response_norm(const math::vector<double>& y
+                                     , math::vector<double>& ynorm);
+    size_t response_norm_size();
+    void solution_not_found();
+    void step_back(double new_step);
+
+
+
+    void linear_like_solve(math::vector_t<double,2>& stif, math::vector<double>& y);
+    void update_Rsum(math::vector<double>& q);
+    void update_loadExt_vector(double new_loadExt_norm);
+
+private:
+    void prepare_fun_jac(math::vector<double>& fun
+                        ,math::vector_t<double,2>& jac);
+    void zeros_stiffness_matrix(math::vector_t<double,2>& stif);
+
+};
+
+
+
+} // namespace fem::npath
---
+++ cpp/fem/source/staticNL.cpp
@@ -0,0 +1,247 @@
+#include "staticNL.hpp"
+
+namespace fem::npath {
+
+StaticNL::StaticNL(const StaticNL& other)
+        : model(other.model)
+        , assemble(other.assemble)
+        
+        , loadExt(other.loadExt)
+        , loadInt(other.loadInt)
+        , Rsum(other.Rsum)
+        , ynorm_pos(other.ynorm_pos)
+        
+        , loadExt_norm(other.loadExt_norm)
+        
+        , Rsum_continuation(other.Rsum_continuation)
+        
+        , temp_Dy(other.temp_Dy)
+        , temp_theta(other.temp_theta)
+        , temp_rotTensor(other.temp_rotTensor)
+        , temp_Rsumi(other.temp_Rsumi)
+{}
+
+StaticNL::StaticNL(StaticNL&& other)
+        : model(std::move(other.model))
+        , assemble(std::move(other.assemble))
+        
+        , loadExt(std::move(other.loadExt))
+        , loadInt(other.loadInt)
+        , Rsum(std::move(other.Rsum))
+        , ynorm_pos(std::move(other.ynorm_pos))
+        
+        , loadExt_norm(other.loadExt_norm)
+        
+        , Rsum_continuation(std::move(other.Rsum_continuation))
+        
+        , temp_Dy(std::move(other.temp_Dy))
+        , temp_theta(std::move(other.temp_theta))
+        , temp_rotTensor(std::move(other.temp_rotTensor))
+        , temp_Rsumi(std::move(other.temp_Rsumi))
+{}
+
+StaticNL::StaticNL(const Model& model
+                  ,const Assemble& assemble
+                  ,const math::vector<size_t>& ynorm_pos)
+        : model(model)
+        , assemble(assemble)
+        , ynorm_pos(ynorm_pos)        
+{
+    initialization();
+}
+
+void StaticNL::initialization() {
+    AnalysisTraits::setup_analysis(model,assemble);
+
+    loadExt = math::zeros<double>(assemble.ndofs);
+    ModelTraits::store_load_vector(model,assemble,loadExt);
+    loadExt_norm = norm(loadExt);
+    
+    loadInt = math::zeros<double>(assemble.ndofs);
+    Rsum = AnalysisTraits::setup_Rsum(model);
+    
+    temp_Dy = math::zeros<double>(assemble.ndofs+1);
+    temp_theta = math::zeros<double>(3);
+    temp_rotTensor = math::zeros<double>(3,3);
+    temp_Rsumi = math::zeros<double>(3,3);
+
+    loadExt_unit = loadExt/loadExt_norm;
+    
+}
+
+
+
+
+
+void StaticNL::continuation_initialization(math::vector_t<double,2>& jac
+                                          ,math::vector<double>& ynorm) {
+    Rsum_continuation = math::vector_t<double,4>();
+    
+    // last jac column is constant and equal to negative unary external load
+    size_t last = assemble.ndofs;
+    auto jac_row = jac.begin();
+    auto load_ext = loadExt.begin(), load_ext_end = loadExt.end();
+    while (load_ext < load_ext_end) {
+        (*jac_row)[last] = -(*load_ext)/loadExt_norm;
+        ++jac_row; ++load_ext;
+    }
+
+    // set ynorm size
+    ynorm.resize(ynorm_pos.size());
+}
+
+
+void StaticNL::process_total_increment(math::vector<double>& y
+                                     , math::vector<double>& dy) {
+    update_Rsum(dy);
+}
+
+void StaticNL::process_total_increment(math::vector<double>& Dy
+                                     , math::vector<double>& dy
+                               , const math::vector<double>& predictor
+                               ,                    double   ds) {
+#if 1
+    double dot_pre_Dy = math::dot(predictor,Dy)
+         , dot_pre_dy = math::dot(predictor,dy);
+    
+    if (dot_pre_Dy+dot_pre_dy <= 0) {
+        double backward_coef = -dot_pre_Dy/dot_pre_dy * ::npath::PathFollowing::track_backward;
+        Dy + dy*backward_coef;
+    } else {
+        math::sum(Dy.begin(),dy.begin(),temp_Dy.begin(),temp_Dy.end());
+    }
+#else
+    temp_Dy = Dy + dy;
+#endif
+
+    dy = temp_Dy*(ds/math::norm(temp_Dy)) - Dy;
+
+    update_Rsum(dy);
+}
+
+
+void StaticNL::system_response(math::vector<double>& fun
+                             , math::vector_t<double,2>& jac
+                             , math::vector<double>& y)
+{   
+    
+    update_loadExt_vector(y.last());
+    
+    // assembleNL uses y by only indeses of q (displacement vector)
+    prepare_fun_jac(fun,jac);
+    ModelTraits::do_assemble_nonlinear(model,assemble,jac,loadInt,y,Rsum,&BaseElement::tangentStiffness_innerLoad);
+    
+    // Calculate fun = loadExt - loadInt
+    math::sub(loadExt.begin(), loadInt.begin(), fun.begin(), fun.begin()+loadExt.size());
+
+    // std::cout << "# stif[0] = " << math::Slice(jac[0].begin(),jac[0].end()) << "\n";
+    
+}
+
+void StaticNL::system_response_extended(math::vector<double>& fun
+                                      , math::vector_t<double,2>& jac
+                                      , math::vector<double>& y)
+{   
+    system_response(fun,jac,y);
+}
+
+
+void StaticNL::calculate_response_norm(const math::vector<double>& y
+                                           , math::vector<double>& ynorm)
+{
+    auto it_ynorm = ynorm.begin()
+        ,it_norm_end = ynorm.end();
+    auto it_ynorm_pos = ynorm_pos.begin();
+
+    while (it_ynorm < it_norm_end) {
+        *it_ynorm = y[*it_ynorm_pos];
+        ++it_ynorm; ++it_ynorm_pos;
+    }
+
+    Rsum_continuation.push_back(Rsum);
+}
+
+size_t StaticNL::response_norm_size() {
+    return ynorm_pos.size();
+}
+
+
+
+
+void StaticNL::linear_like_solve(math::vector_t<double,2>& stif, math::vector<double>& y) {
+    update_loadExt_vector(y.last());
+    
+    ModelTraits::do_assemble_nonlinear(model,assemble,stif,loadInt,y,Rsum,&BaseElement::tangentStiffness_innerLoad);
+    
+    math::solve_ldlt(stif,loadExt,y);
+}
+
+void StaticNL::update_Rsum(math::vector<double>& q) {
+    AnalysisTraits::update_Rsum(model,assemble,Rsum,q
+                            ,temp_theta,temp_rotTensor,temp_Rsumi);
+}
+
+void StaticNL::update_loadExt_vector(double new_loadExt_norm) {
+	auto load_node     = model.loads_info.begin()
+		,load_node_end = model.loads_info.end();
+		
+	decltype(model.loads_info[0].dofs.begin()) load_dof, load_dof_end;
+	
+	decltype(assemble.nodes_dofs.begin()) gnode;
+	decltype(assemble.nodes_dofs[0].begin()) gdof;
+	double loadExt_norm_frac = new_loadExt_norm/loadExt_norm;
+	while (load_node != load_node_end) {
+		load_dof = load_node->dofs.begin();
+		load_dof_end = load_node->dofs.end();
+		gdof = assemble.nodes_dofs[load_node->id].begin();
+		while (load_dof != load_dof_end) {
+			if (*gdof > 0) {
+                *load_dof *= loadExt_norm_frac;
+				loadExt[*gdof-1] = *load_dof;
+			}
+			++load_dof; ++gdof;
+		}
+		++load_node;
+	}
+    loadExt_norm = new_loadExt_norm;
+
+}
+
+void StaticNL::solution_not_found() {
+    std::copy(Rsum_continuation.last().begin(),Rsum_continuation.last().end(),Rsum.begin());
+    // Rsum = Rsum_continuation.last();
+}
+
+void StaticNL::step_back(double) {
+    Rsum_continuation.erase(Rsum_continuation.end()-1);
+    std::copy(Rsum_continuation.last().begin(),Rsum_continuation.last().end(),Rsum.begin());
+    // Rsum = *(Rsum_continuation.end()-1);
+    // std::cout << "# SRsum = \n" << Rsum << std::endl;
+    // throw 1;
+}
+
+
+void StaticNL::prepare_fun_jac(math::vector<double>& fun
+                              ,math::vector_t<double,2>& jac) {
+    math::fill(loadInt.begin(),loadInt.end(), 0.0);
+    
+    zeros_stiffness_matrix(jac);
+}
+
+void StaticNL::zeros_stiffness_matrix(math::vector_t<double,2>& stif) {
+    for (auto& row: stif) {
+        math::fill(row.begin(),row.begin()+assemble.ndofs, 0.0);
+    }
+    // stif = zeros<double>(stif);
+
+    // // last jac column is constant and equal to negative unary external load
+    // size_t last = assemble.ndofs;
+    // auto jac_row = stif.begin();
+    // auto load_ext = loadExt_unit.begin(), load_ext_end = loadExt_unit.end();
+    // while (load_ext < load_ext_end) {
+    //     (*jac_row)[last] = -(*load_ext);
+    //     ++jac_row; ++load_ext;
+    // }
+}
+
+} // namespace fem::npath
