26d81e699543ac23b175a6fcd9dd3488845d4fc9
Alikin Nikolay <nickalikin@gmail.com>
2025-01-27 17:29:02 +0300

    Internal namespace::fem file with basic declaretions

---
+++ cpp/fem/include/fem_base.hpp
@@ -0,0 +1,20 @@
+/*Base FEM module with all declarations*/
+
+#pragma once
+
+#include "linalg.hpp"
+
+
+namespace fem {
+
+using block_t = math::vector< math::Slice<typename math::vector<double>::iterator,
+						                  typename math::vector<double>::iterator> >;
+using const_block_t = math::vector< math::Slice<typename math::vector<double>::const_iterator,
+												typename math::vector<double>::const_iterator> >;
+
+class Model;
+class Assemble;
+struct ModelTraits;
+struct AnalysisTraits;
+
+} // namespace fem
