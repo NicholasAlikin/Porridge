33ad09f6b81a2945fb420adf51ac5d561a647128
Alikin Nikolay <nickalikin@gmail.com>
2025-05-13 17:00:20 +0300

    Edited DFT method to template VectorLike. Added method for FEM HBM

--- cpp/harmonic_balance/include/dft.hpp
+++ cpp/harmonic_balance/include/dft.hpp
@@ -10,17 +10,34 @@ public:
     size_t N = 0;
     size_t ndof = 1;
 
-    // math::vector_t<double,2> forward_basic;     // for ndof = 1
+    // vector transform for ndof = 1
     math::vector_t<double,2> forward_basic;
     math::vector_t<double,2> backward_basic;
     math::vector_t<double,2> derivative_basic;  // for ndof = 1
     math::vector_t<double,2> derivative2_basic; // for ndof = 1
+    // matrix transform for ndof = 1
+// private:
+    mutable math::vector_t<double,3> forward_matrix;
+    mutable math::vector_t<double,3> forward_matrix_dot_derivative;
+    mutable math::vector_t<double,3> forward_matrix_dot_derivative2;
 
-    // math::vector_t<double,2> forward;
-    // math::vector_t<double,2> backward;
-    // math::vector_t<double,2> derivative;
-    // math::vector_t<double,2> derivative2;
+    /* Tangent matrices in frequency domain */
+    mutable math::vector_t<double,2> mass_freq;
+    mutable math::vector_t<double,2> damp_freq;
+    mutable math::vector_t<double,2> stif_freq;
 
+    /* Other */
+    mutable math::vector_t<double,2> derivative_forward_helper;
+    mutable math::vector<double> lb0_helper;
+
+
+public:
+    
+
+    size_t frequency_size() const;
+    size_t frequency_basic_size() const;
+    size_t time_size() const;
+    size_t time_basic_size() const;
 
     static math::vector_t<double,2> calculate_forward_basic(size_t H, size_t N);
     static math::vector_t<double,2> calculate_backward_basic(size_t H, size_t N);
@@ -28,28 +45,361 @@ public:
     static math::vector_t<double,2> calculate_derivative2_basic(size_t H, size_t N);
     static math::vector_t<double,2> calculate_derivative2_basic(const math::vector_t<double,2>& T_derivative_basic);
 
-
-    static math::vector_t<double,2> calculate_forward(size_t H, size_t N, size_t ndof);
-    static math::vector_t<double,2> calculate_backward(size_t H, size_t N, size_t ndof);
-    static math::vector_t<double,2> calculate_derivative(size_t H, size_t N, size_t ndof);
-
+    static math::vector_t<double,3> calculate_forward_matrix_basic(size_t H, size_t N);
 
     DFT() = default;
     DFT(size_t H, size_t N, size_t ndof = 1);
+    DFT(const DFT& other);
+    DFT(DFT&& other);
 
 private:
-    math::vector<double> transform(const math::vector_t<double,2>& transform_matrix, size_t res_size, const math::vector<double>& vec) const;
-    void __derivative(math::vector<double>& res) const;
-    void __derivative2(math::vector<double>& res) const;
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void transform(const math::vector_t<double,2>& transform_matrix,
+                   const V1& vec, V2& res, size_t ndofs) const;
+    
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void transform(const math::vector_t<double,2>& transform_matrix,
+                   const V1& vec, V2& res) const;
+
+
+    template <math::Vector V>
+    void __derivative(V& res, size_t ndofs) const;
+    
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void __derivative(const V1& vec, V2& res, size_t ndofs) const;
+    
+    
+    template <math::Vector V>
+    void __derivative2(V& res) const;
+    
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void __derivative2(const V1& vec, V2& res) const;
+
 public:
-    math::vector<double> forward(const math::vector<double>& vec) const;
-    math::vector<double> backward(const math::vector<double>& vec) const;
-    math::vector<double> derivative(const math::vector<double>& vec) const;
+    
+    template <math::Vector V>
+    math::vector<double> forward(const V& vec) const;
+    
+    
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void                 forward(const V1& vec, V2& res, size_t ndofs) const;
+
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void                 forward(const V1& vec, V2& res) const;
+    
+
+    template <math::Vector V>
+    math::vector<double> backward(const V& vec) const;
+
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void                 backward(const V1& vec, V2& res, size_t ndofs) const;
+
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void                 backward(const V1& vec, V2& res) const;
+
+
+    template <math::Vector V>
+    math::vector<double> derivative(const V& vec) const;
+
+    template <math::Vector V>
+    math::vector<double> derivative(const V& vec, size_t ndofs) const;
+
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void                 derivative(const V1& vec, V2& res) const;
+
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void                 derivative(const V1& vec, V2& res, size_t ndofs) const;
+
+
     math::vector<double> derivative(math::vector<double>&& vec) const;
-    math::vector<double> derivative2(const math::vector<double>& vec) const;
+
+
+    math::vector<double> derivative(math::vector<double>&& vec, size_t ndofs) const;
+    
+
+    template <math::Vector V>
+    math::vector<double> derivative2(const V& vec) const;
+
+
+    template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+    void                 derivative2(const V1& vec, V2& res) const;
+
     math::vector<double> derivative2(math::vector<double>&& vec) const;
     
 
+    void jac_time_domain_precomp(size_t base_ndof);
+    void jac_time_domain_inc(math::vector<math::vector_slice<double>>& matrix
+                            ,const math::vector_t<double,2>& mass
+                            ,const math::vector_t<double,2>& damp
+                            ,const math::vector_t<double,2>& stif
+                            , double freq, size_t time) const;
+
+    /* Iteration of calculation Jacobi matrix of system in frequency domain as
+        1. Transfrorm state vector from frequency to time domain,
+        2. Calculate tangent matrices `mass`, `damp` and `stif` in time domain
+        3. For each `time` sample call this method for the next iteration
+    Also iteration of calculation ... */
+    void jac_extended_time_domain_inc(math::vector<math::vector_slice<double>>& drdq
+                               ,const math::vector_t<double,2>& mass
+                               ,const math::vector_t<double,2>& damp
+                               ,const math::vector_t<double,2>& stif
+                               ,double freq, size_t time
+                                    , math::vector_slice<double>& drdw
+                               ,const math::vector_const_slice<double>& q
+                               ,const math::vector_const_slice<double>& r) const;
+
+    void time_domain(const math::vector<double>& q, double freq
+                          ,math::vector<double>& u
+                          ,math::vector<double>& dudt
+                          ,math::vector<double>& d2udt2, size_t ndof) const;
+
+
 };
+
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::transform(const math::vector_t<double,2>& transform_matrix
+                  , const V1& vec, V2& res, size_t ndofs) const
+{
+    math::Slice sl_res(res.begin(), res.end(), ndofs);
+    math::Slice sl_vec(vec.begin(), vec.end(), ndofs);
+    
+    for (size_t dof = 0; dof < ndofs; ++dof) {
+        sl_res.update_from(res.begin()+dof); // slices size remains constant
+        sl_vec.update_from(vec.begin()+dof);
+        math::dot(transform_matrix,sl_vec,sl_res);
+    }
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::transform(const math::vector_t<double,2>& transform_matrix
+                  , const V1& vec, V2& res) const
+{
+    transform(transform_matrix, vec, res, ndof);
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::forward(const V1& vec, V2& res, size_t ndofs) const {
+    transform(forward_basic, vec, res, ndofs);
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::forward(const V1& vec, V2& res) const {
+    forward(vec, res, ndof);
+}
+
+
+template <math::Vector V>
+math::vector<double> DFT::forward(const V& vec) const {
+    auto res = math::zeros<double>(frequency_size());
+    forward(vec,res);
+    return res;
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::backward(const V1& vec, V2& res, size_t ndofs) const {
+    transform(backward_basic,vec,res,ndofs);
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::backward(const V1& vec, V2& res) const {
+    backward(vec,res,ndof);
+}
+
+
+template <math::Vector V>
+math::vector<double> DFT::backward(const V& vec) const {
+    auto res = math::zeros<double>(time_size());
+    backward(vec,res);
+    return res;
+}
+
+
+
+template <math::Vector V>
+void DFT::__derivative(V& res, size_t ndofs) const {
+    auto res_dof = res.begin();
+    math::Slice sl_cos(res_dof+1*ndofs, res.end(), 2*ndofs)
+              , sl_sin(res_dof+2*ndofs, res.end(), 2*ndofs);
+    decltype(sl_cos.begin()) it_cos,it_sin;
+    size_t h;
+    double tmp;
+    for (size_t dof = 0; dof < ndofs; ++dof) {
+        *res_dof = 0;
+        h = 1;
+        sl_cos.update_from(res_dof+1*ndofs);
+        sl_sin.update_from(res_dof+2*ndofs);
+        it_cos = sl_cos.begin();
+        it_sin = sl_sin.begin();
+        while (h <= H) {
+            tmp = *it_cos;
+            *it_cos = (*it_sin)*h;
+            *it_sin = -tmp*h;
+            ++h; ++it_cos; ++it_sin;
+        }
+        ++res_dof;
+    }
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::__derivative(const V1& vec, V2& res, size_t ndofs) const {
+    auto vec_dof = vec.begin();
+    auto res_dof = res.begin();
+
+    math::Slice sl0_cos(vec_dof+1*ndofs, vec.end(), 2*ndofs);
+    math::Slice sl_cos( res_dof+1*ndofs, res.end(), 2*ndofs)
+              , sl_sin( res_dof+2*ndofs, res.end(), 2*ndofs);
+    
+    decltype(sl0_cos.begin()) it0_cos,it0_sin;
+    decltype(sl_cos.begin()) it_cos,it_sin;
+    
+    size_t h;
+    for (size_t dof = 0; dof < ndofs; ++dof) {
+        *res_dof = 0;
+        
+        sl0_cos.update_from(vec_dof+1*ndofs);
+        it0_cos = sl0_cos.begin();
+        
+        sl_cos.update_from(res_dof+1*ndofs);
+        sl_sin.update_from(res_dof+2*ndofs);
+        it_cos = sl_cos.begin();
+        it_sin = sl_sin.begin();
+
+        for (h = 1; h <= H; ++h
+                            ,++it0_cos
+                            ,++it_cos
+                            ,++it_sin)
+        {
+            *it_cos =  (*it_sin )*h;
+            *it_sin = -(*it0_cos)*h;
+        }
+        ++res_dof;
+        ++vec_dof;
+    }
+}
+
+
+template <math::Vector V>
+math::vector<double> DFT::derivative(const V& vec) const {
+    math::vector<double> res = vec;
+    __derivative(vec,res,ndof);
+    return res;
+}
+
+
+template <math::Vector V>
+math::vector<double> DFT::derivative(const V& vec, size_t ndofs) const {
+    math::vector<double> res = vec;
+    __derivative(vec,res,ndofs);
+    return res;
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::derivative(const V1& vec, V2& res) const {
+    derivative(vec,res,ndof);
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::derivative(const V1& vec, V2& res, size_t ndofs) const {
+    __derivative(vec,res,ndofs);
+}
+
+
+template <math::Vector V>
+void DFT::__derivative2(V& res) const {
+    auto res_dof = res.begin();
+    math::Slice sl_cos(res_dof+1*ndof, res.end(), 2*ndof)
+              , sl_sin(res_dof+2*ndof, res.end(), 2*ndof);
+    decltype(sl_cos.begin()) it_cos,it_sin;
+    size_t h;
+    for (size_t dof = 0; dof < ndof; ++dof) {
+        *res_dof = 0;
+        h = 1;
+        sl_cos.update_from(res_dof+1*ndof);
+        sl_sin.update_from(res_dof+2*ndof);
+        it_cos = sl_cos.begin();
+        it_sin = sl_sin.begin();
+        while (h <= H) {
+            (*it_cos) *= -h*h;
+            (*it_sin) *= -h*h;
+            ++h; ++it_cos; ++it_sin;
+        }
+        ++res_dof;
+    }
+}
+
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::__derivative2(const V1& vec, V2& res) const {
+    auto vec_dof = vec.begin();
+    auto res_dof = res.begin();
+    
+    math::Slice sl0_cos(vec_dof+1*ndof, vec.end(), 2*ndof)
+              , sl0_sin(vec_dof+2*ndof, vec.end(), 2*ndof);
+    math::Slice sl_cos( res_dof+1*ndof, res.end(), 2*ndof)
+              , sl_sin( res_dof+2*ndof, res.end(), 2*ndof);
+    
+    decltype(sl0_cos.begin()) it0_cos,it0_sin;
+    decltype(sl_cos.begin() ) it_cos, it_sin;
+    size_t h;
+    for (size_t dof = 0; dof < ndof; ++dof) {
+        *res_dof = 0;
+        
+        sl0_cos.update_from(vec_dof+1*ndof);
+        sl0_sin.update_from(vec_dof+2*ndof);
+        it0_cos = sl0_cos.begin();
+        it0_sin = sl0_sin.begin();
+
+        sl_cos.update_from(res_dof+1*ndof);
+        sl_sin.update_from(res_dof+2*ndof);
+        it_cos = sl_cos.begin();
+        it_sin = sl_sin.begin();
+        
+        for (h = 1; h <= H; ++h
+                           ,++it0_cos
+                           ,++it0_sin
+                           ,++it_cos
+                           ,++it_sin)
+        {
+            *it_cos = -h*h* (*it0_cos);
+            *it_sin = -h*h* (*it0_sin);
+        }
+
+        ++vec_dof;
+        ++res_dof;
+    }
+}
+
+
+template <math::Vector V>
+math::vector<double> DFT::derivative2(const V& vec) const {
+    math::vector<double> res = vec;
+    __derivative2(vec,res);
+    return res;
+}
+
+
+template <math::Vector V1, math::ArithmeticVectorsLike<V1> V2>
+void DFT::derivative2(const V1& vec, V2& res) const {
+    __derivative2(vec,res);
+}
 
 } // namespace npath
--- cpp/harmonic_balance/source/dft.cpp
+++ cpp/harmonic_balance/source/dft.cpp
@@ -2,6 +2,19 @@
 
 namespace npath {
 
+size_t DFT::frequency_basic_size() const {
+    return 2*H+1;
+}
+size_t DFT::frequency_size() const {
+    return frequency_basic_size()*ndof;
+}
+
+size_t DFT::time_basic_size() const {
+    return N;
+}
+size_t DFT::time_size() const {
+    return time_basic_size()*ndof;
+}
 
 math::vector_t<double,2> DFT::calculate_forward_basic(size_t H, size_t N) {
     math::vector_t<double,2> T_forward = math::zeros<double>(2*H+1, N);
@@ -30,13 +43,6 @@ math::vector_t<double,2> DFT::calculate_backward_basic(size_t H, size_t N) {
     return T_backward;
 }
 
-math::vector_t<double,2> DFT::calculate_forward(size_t H, size_t N, size_t ndof) {
-    return math::kron(DFT::calculate_forward_basic(H,N), math::eye<double>(ndof));
-}
-math::vector_t<double,2> DFT::calculate_backward(size_t H, size_t N, size_t ndof) {
-    return math::kron(DFT::calculate_backward_basic(H,N), math::eye<double>(ndof));
-}
-
 math::vector_t<double,2> DFT::calculate_derivative_basic(size_t H, size_t) {
     math::vector_t<double,2> T_derivative = math::zeros<double>(2*H+1, 2*H+1);
     double k = 1;
@@ -56,9 +62,32 @@ math::vector_t<double,2> DFT::calculate_derivative2_basic(const math::vector_t<d
     return dot(T_derivative2_basic,T_derivative2_basic);
 }
 
-math::vector_t<double,2> DFT::calculate_derivative(size_t H, size_t N, size_t ndof) {
-    math::vector_t<double,2> T_derivative = math::kron(DFT::calculate_derivative_basic(H,N), math::eye<double>(ndof));
-    return T_derivative;
+math::vector_t<double,3> DFT::calculate_forward_matrix_basic(size_t H, size_t N) {
+    math::vector_t<double,3> M_forward(N,math::vector_t<double,2>(2*H+1));
+    math::vector<double> l(2*H+1);
+    l[0] = 1.0;
+
+    auto itl_end = l.end();
+    decltype(itl_end) itl;
+    decltype(M_forward[0].begin()) itM;
+
+    for (size_t n = 0; n < N; ++n) {
+        // store l vector
+        for (size_t h = 1; h <= H; ++h) {
+            l[2*h-1] = std::cos(h* 2*math::pi/N * n);
+            l[2*h]   = std::sin(h* 2*math::pi/N * n);
+        }
+        M_forward[n][0] = 0.5*l;
+        itl = l.begin() + 1;
+        itM = M_forward[n].begin()+1;
+        while (itl < itl_end) {
+            *itM = (*itl)*l;
+            ++itl;
+            ++itM;
+        }
+    }
+
+    return M_forward*(2./N);
 }
 
 DFT::DFT(size_t H, size_t N, size_t ndof)
@@ -67,99 +96,183 @@ DFT::DFT(size_t H, size_t N, size_t ndof)
         , backward_basic(DFT::calculate_backward_basic(H,N))
         , derivative_basic(DFT::calculate_derivative_basic(H,N))
         , derivative2_basic(DFT::calculate_derivative2_basic(derivative_basic))
-        {
-}
+{}
 
 
-math::vector<double> DFT::transform(const math::vector_t<double,2>& transform_matrix, size_t res_size, const math::vector<double>& vec) const {
-    
-    math::vector<double> res = math::zeros<double>(res_size);
-    math::Slice<decltype(res.begin()),decltype(res.end())> sl_res;
-    math::Slice<decltype(vec.begin()),decltype(vec.end())> sl_vec;
-    
-    for (size_t dof = 0; dof < ndof; ++dof){
-        sl_res.new_slice(res.begin()+dof, res.end(), ndof);
-        sl_vec.new_slice(vec.begin()+dof, vec.end(), ndof);
-        math::dot(transform_matrix,sl_vec,sl_res);
-    }
-    return res;
-}
+DFT::DFT(const DFT& other)
+        : H(other.H), N(other.N), ndof(other.ndof)
+        , forward_basic(other.forward_basic)
+        , backward_basic(other.backward_basic)
+        , derivative_basic(other.derivative_basic)
+        , derivative2_basic(other.derivative2_basic)
 
-math::vector<double> DFT::forward(const math::vector<double>& vec) const {
-    /*vec.size() == N ndof, res.size() == (2H+1)ndof*/
-    return transform(forward_basic,(2*H+1)*ndof,vec);
-}
+        , forward_matrix(other.forward_matrix)
+        , forward_matrix_dot_derivative(other.forward_matrix_dot_derivative)
+        , forward_matrix_dot_derivative2(other.forward_matrix_dot_derivative2)
 
-math::vector<double> DFT::backward(const math::vector<double>& vec) const {
-    /*vec.size() == (2H+1)ndof, res.size() == N ndof*/
-    return transform(backward_basic,N*ndof,vec);
-}
+        , mass_freq(other.mass_freq)
+        , damp_freq(other.damp_freq)
+        , stif_freq(other.stif_freq)
 
-void DFT::__derivative(math::vector<double>& res) const {
-    auto res_dof = res.begin();
-    math::Slice<decltype(res.begin()),decltype(res.end())> sl_cos, sl_sin;
-    decltype(sl_cos.begin()) it_cos,it_sin;
-    size_t h;
-    double tmp;
-    for (size_t dof = 0; dof < ndof; ++dof) {
-        *res_dof = 0;
-        h = 1;
-        sl_cos.new_slice(res_dof+1*ndof, res.end(), 2*ndof);
-        sl_sin.new_slice(res_dof+2*ndof, res.end(), 2*ndof);
-        it_cos = sl_cos.begin();
-        it_sin = sl_sin.begin();
-        while (h <= H) {
-            tmp = *it_cos;
-            *it_cos = (*it_sin)*h;
-            *it_sin = -tmp*h;
-            ++h; ++it_cos; ++it_sin;
-        }
-        ++res_dof;
-    }
-}
+        , derivative_forward_helper(other.derivative_forward_helper)
+        , lb0_helper(other.lb0_helper)
+{}
 
 
-math::vector<double> DFT::derivative(const math::vector<double>& vec) const {
-    math::vector<double> res = vec;
-    __derivative(res);
-    return res;
-}
+DFT::DFT(DFT&& other)
+        : H(other.H), N(other.N), ndof(other.ndof)
+        , forward_basic(std::move(other.forward_basic))
+        , backward_basic(std::move(other.backward_basic))
+        , derivative_basic(std::move(other.derivative_basic))
+        , derivative2_basic(std::move(other.derivative2_basic))
+
+        , forward_matrix(std::move(other.forward_matrix))
+        , forward_matrix_dot_derivative(std::move(other.forward_matrix_dot_derivative))
+        , forward_matrix_dot_derivative2(std::move(other.forward_matrix_dot_derivative2))
+
+        , mass_freq(std::move(other.mass_freq))
+        , damp_freq(std::move(other.damp_freq))
+        , stif_freq(std::move(other.stif_freq))
+
+        , derivative_forward_helper(std::move(other.derivative_forward_helper))
+        , lb0_helper(std::move(other.lb0_helper))
+{}
+
 
 math::vector<double> DFT::derivative(math::vector<double>&& vec) const {
+    return derivative(std::move(vec), ndof);
+}
+
+
+math::vector<double> DFT::derivative(math::vector<double>&& vec, size_t ndofs) const {
     math::vector<double> res = std::move(vec);
-    __derivative(res);
+    __derivative(res, ndofs);
     return res;
 }
 
-void DFT::__derivative2(math::vector<double>& res) const {
-    auto res_dof = res.begin();
-    math::Slice<decltype(res.begin()),decltype(res.end())> sl_cos, sl_sin;
-    decltype(sl_cos.begin()) it_cos,it_sin;
-    size_t h;
-    for (size_t dof = 0; dof < ndof; ++dof) {
-        *res_dof = 0;
-        h = 1;
-        sl_cos.new_slice(res_dof+1*ndof, res.end(), 2*ndof);
-        sl_sin.new_slice(res_dof+2*ndof, res.end(), 2*ndof);
-        it_cos = sl_cos.begin();
-        it_sin = sl_sin.begin();
-        while (h <= H) {
-            (*it_cos) *= -h*h;
-            (*it_sin) *= -h*h;
-            ++h; ++it_cos; ++it_sin;
-        }
-        ++res_dof;
-    }
-}
-math::vector<double> DFT::derivative2(const math::vector<double>& vec) const {
-    math::vector<double> res = vec;
-    __derivative2(res);
-    return res;
-}
+
 
 math::vector<double> DFT::derivative2(math::vector<double>&& vec) const {
     math::vector<double> res = std::move(vec);
     __derivative2(res);
     return res;
 }
+
+
+void DFT::jac_time_domain_precomp(size_t base_ndof) {
+    forward_matrix = DFT::calculate_forward_matrix_basic(H,N);
+    forward_matrix_dot_derivative  = math::zeros<double>(forward_matrix);
+    forward_matrix_dot_derivative2 = math::zeros<double>(forward_matrix);
+
+    for (size_t t = 0; t < N; ++t) {
+        forward_matrix_dot_derivative[t] = math::dot(forward_matrix[t],
+                                                     derivative_basic);
+
+        forward_matrix_dot_derivative2[t] = math::dot(forward_matrix[t],
+                                                      derivative2_basic);
+    }
+    
+    mass_freq = math::zeros<double>(base_ndof*frequency_basic_size(),
+                                    base_ndof*frequency_basic_size());
+    damp_freq = math::zeros<double>(base_ndof*frequency_basic_size(),
+                                    base_ndof*frequency_basic_size());
+    stif_freq = math::zeros<double>(base_ndof*frequency_basic_size(),
+                                    base_ndof*frequency_basic_size());
+    
+    /**/
+    derivative_forward_helper = math::zeros<double>(N,frequency_basic_size());
+    math::vector<double> lb(2*H+1);
+    lb[0] = 0.5;
+
+    auto itlb_end = lb.end();
+    decltype(itlb_end) itl;
+    
+    for (size_t n = 0; n < N; ++n) { 
+        // store l vector
+        for (size_t h = 1; h <= H; ++h) {
+            lb[2*h-1] = std::cos(h* 2.*math::pi/N * n);
+            lb[2*h]   = std::sin(h* 2.*math::pi/N * n);
+        }
+        if (n == 0) {
+            lb0_helper = lb*(-2.);
+            continue; // derivative_forward_helper[n = 0] == 0
+        }
+        math::dot(derivative_basic,lb*(2./N* 2.*math::pi*n/N)
+                  ,derivative_forward_helper[n]);
+    }
+}
+
+void DFT::jac_time_domain_inc(math::vector<math::vector_slice<double>>& matrix
+                            ,const math::vector_t<double,2>& mass
+                            ,const math::vector_t<double,2>& damp
+                            ,const math::vector_t<double,2>& stif
+                            ,double freq, size_t time) const
+{
+    if (time == 0) {
+        math::fill(mass_freq,0.0);
+        math::fill(damp_freq,0.0);
+        math::fill(stif_freq,0.0);
+    }
+
+    math::addkron(forward_matrix_dot_derivative2[time], mass, mass_freq);
+    math::addkron(forward_matrix_dot_derivative[time], damp, damp_freq);
+    math::addkron(forward_matrix[time], stif, stif_freq);
+    
+
+    /* last loop iteration */
+    if (time == N-1) {
+        matrix = (mass_freq*(freq*freq) + damp_freq*(freq) + stif_freq);
+    }
+}
+
+    
+void DFT::jac_extended_time_domain_inc(math::vector<math::vector_slice<double>>& drdq
+                                ,const math::vector_t<double,2>& mass
+                                ,const math::vector_t<double,2>& damp
+                                ,const math::vector_t<double,2>& stif
+                                ,double freq, size_t time
+                                     , math::vector_slice<double>& drdw
+                                ,const math::vector_const_slice<double>& q
+                                ,const math::vector_const_slice<double>& r) const
+{
+    if (time == 0) {
+        math::fill(mass_freq,0.0);
+        math::fill(damp_freq,0.0);
+        math::fill(stif_freq,0.0);
+    }
+
+    math::addkron(forward_matrix_dot_derivative2[time], mass, mass_freq);
+    math::addkron(forward_matrix_dot_derivative[time], damp, damp_freq);
+    math::addkron(forward_matrix[time], stif, stif_freq);
+    
+
+    /* last loop iteration */
+    if (time == N-1) {
+        drdq = (mass_freq*(freq*freq) + damp_freq*(freq) + stif_freq);
+        math::dot((mass_freq*(freq*2) + damp_freq), q, drdw);
+    }
+
+}
+
+
+void DFT::time_domain(const math::vector<double>& q, double freq
+                          ,math::vector<double>& u
+                          ,math::vector<double>& dudt
+                          ,math::vector<double>& d2udt2, size_t ndofs) const
+{   
+    math::fill(u,0.0);
+    math::fill(dudt,0.0);
+    math::fill(d2udt2,0.0);
+    backward(q, u, ndofs);
+    
+    math::vector<double> subq = q*freq;
+    subq = derivative(std::move(subq), ndofs);
+    backward(subq, dudt, ndofs);
+    
+    subq *= freq;
+    subq = derivative(std::move(subq), ndofs);
+    backward(subq, d2udt2, ndofs);
+}
+
+
 } // namespace npath
